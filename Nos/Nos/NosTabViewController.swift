a46ec9ed8b8f0544084bc59f31e4b4e974283148
danilobecke <danilobecke@MacBook-Pro-de-Danilo-2.local>
2016-06-23 17:29:35 -0300

    tabbar layout done

---
+++ Nos/Nos/NosTabViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NosTabViewController.swift
+//  Nos
+//
+//  Created by Danilo Becke on 23/06/16.
+//  Copyright © 2016 Time Nós. All rights reserved.
+//
+
+import UIKit
+
+class NosTabViewController: UITabBarController {
+    
+    @IBOutlet weak var mapButton: UIImageView!
+    @IBOutlet weak var timelineButton: UIImageView!
+    @IBOutlet weak var faqButton: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
---
+++ Nos/NosTests/NosTests.swift
@@ -0,0 +1,36 @@
+//
+//  NosTests.swift
+//  NosTests
+//
+//  Created by Danilo Becke on 23/06/16.
+//  Copyright © 2016 Time Nós. All rights reserved.
+//
+
+import XCTest
+@testable import Nos
+
+class NosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
